e703b105cec1b8a16052c5c3a33e8c8f30a197b0
S3gillu <shubhamsaurabh440@yahoo.com>
2017-09-27 14:57:40 +0530

    Task

---
+++ Task/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserContext context ;
+
+        public HomeController()
+        {
+            context = new UserContext();
+        }
+        public ActionResult Index()
+        {
+            return View(context.Users.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(User custom)
+        {
+
+            context.Users.Add(custom);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User Custom)
+        {   
+            return View();
+        }
+
+
+        
+
+
+    }
+}
---
+++ Task/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Task.Models
+{
+    public class User
+    {   public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        public string Fname { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        public string Lname { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+      
+    }
+}
